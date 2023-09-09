c333dae2a080e8274269895546741d17a7c01d7f
Francisco <jfguzman.8@gmail.com>
2023-09-09 17:10:30 -0300

    templates y contenido para el blog

---
+++ .eleventy.js
@@ -0,0 +1,17 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("./src/style.css");
+  eleventyConfig.addPassthroughCopy("./src/assets");
+
+  //Return your Object options:
+  return {
+    dir: {
+      input: "src",
+      output: "public",
+      /*   includes: "_includes", */
+    },
+    /*     templateFormats: ["md", "njk", "html"],
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk", */
+  };
+};
